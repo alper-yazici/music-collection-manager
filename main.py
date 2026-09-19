ebde40edc70b89b82cf477a75d5007a13b94d476
Alper Yazici <yazici.alper89@gmail.com>
2026-09-19 06:24:04 +0200

    Add album search and fix vinyl field order

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from vinyl import Vinyl
 from music_collection import MusicCollection
 
-record1 = Vinyl("Daft Punk", "Random Access Memories", 2013, "Electronic", "Columbia", "Germany", date(2026, 8, 14), Decimal("29.99"), "Bought new", Decimal("35.00"), "Shelf 1", "88883716861", "Black", "Original Pressing", "Near Mint", 12, 33, "LP")
+record1 = Vinyl("Daft Punk", "Random Access Memories", 2013, "Electronic", "Columbia", "Germany", date(2026, 8, 14), Decimal("29.99"), "Bought new", Decimal("35.00"), "Near Mint", "Shelf 1", "88883716861", "Black", "Original Pressing", 12, 33, "LP")
 
 record2 = Vinyl("Sezen Aksu", "Sen Aglama", 1984, "Pop", "Sembol", "Turkey", date(2025, 8, 15), Decimal("24.99"), "Bought used", Decimal("40.00"), "Very Good Plus", "Shelf 2", "SEMBOL-001", "Black", "Original Pressing", 12, 33, "LP")
 
@@ -19,3 +19,15 @@ if results:
         print(item)
 else:
     print("Artist not found.")
+
+
+
+print("\nAlbum search result:")
+
+album_results = collection.find_by_album("Sen Aglama")
+
+if album_results:
+    for item in album_results:
+        print(item)
+else:
+    print("Album not found.")
