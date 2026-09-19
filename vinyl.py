8b77161182d42a5f4b745dc1cc5ab29b8056d985
Alper Yazici <317040402+alper-yazici@users.noreply.github.com>
2026-09-19 06:56:30 +0200

    Add album search and fix vinyl field order

--- vinyl.py
+++ vinyl.py
@@ -1,10 +1,8 @@
 from physical_media import PhysicalMedia
-from datetime import date
-from decimal import Decimal
 
 class Vinyl(PhysicalMedia):
-    def __init__(self, artist, album, release_year, genre, label, country, purchase_date, purchase_price, notes, current_market_value, location, catalog_number, color, edition, condition, record_size, speed, format):
-        super().__init__(artist, album, release_year, genre, label, country, purchase_date, purchase_price, notes, current_market_value, location, catalog_number, color, edition, condition)
+    def __init__(self, artist, album, release_year, genre, label, country, purchase_date, purchase_price, notes, current_market_value, condition, location, catalog_number, color, edition, record_size, speed, format):
+        super().__init__(artist, album, release_year, genre, label, country, purchase_date, purchase_price, notes, current_market_value, condition, location, catalog_number, color, edition)
         self.record_size = record_size
         self.speed = speed
         self.format = format
